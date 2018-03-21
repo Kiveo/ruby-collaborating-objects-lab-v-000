d50e1f573dfa14b1a04c54e789d415da09e8a76b
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 15:06:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
     end
   end
 
-  def new_by_filename(filename)
+  def self.new_by_filename(filename)
     song = self.new #something to parse onto
     song.title = filename.split("-")[1] #parse
     song #new song from filename
