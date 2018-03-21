3d0097c860a1682de0034fbe9848fa35c563af96
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 16:06:11 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,13 +7,12 @@ class MP3Importer
 
   def files
     #load
+    @path.
     #convert to mp3s
   end
 
-  def import(list_of_filenames)
-    list_of_filenames.each {|filename|
-      Song.new_by_filename(filename)
-    }
+  def import
+    list_of_filenames.each { |filename| Song.new_by_filename(filename) }
   end
 
 end
