4d8f845aa6deaab608ba1615ca944795e540f3d1
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 15:15:45 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,14 @@
 class MP3Importer
-  # attr_accessor :filename
+  attr_accessor :filepath
+
+  def initialize(filepath)
+    @filepath = filepath
+  end
+
+  def files
+    #load
+    #convert to mp3s
+  end
 
   def import(list_of_filenames)
     list_of_filenames.each {|filename|
