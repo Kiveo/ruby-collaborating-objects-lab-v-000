aa469344103afff7d11dae6a0527fc337a5e88d7
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 16:17:46 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,16 @@
 class MP3Importer
-  attr_reader :path
+  attr_reader :path, :files
 
   def initialize(path)
     @path = path
   end
 
   def files
-    files = Dir.entries(@path).grep(/.mp3/)
+    @files = Dir.entries(@path).grep(/.mp3/)
   end
 
   def import
-    files.each { |file| Song.new_by_filename(file) }
+    @files.each { |file| Song.new_by_filename(file) }
   end
 
 end
